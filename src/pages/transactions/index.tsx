e43eab0bc982f5ae9e1931f731ab7001c0683f6f
Jean Borges <jeanborges946@gmail.com>
2023-03-21 15:02:35 -0300

    feature: Adding TransactionsTable component

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,11 +1,54 @@
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles';
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width='50%'>Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant='income'>R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>21/03/2023</td>
+            </tr>
+            <tr>
+              <td width='50%'>Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant='income'>R$ 5.320,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>21/03/2023</td>
+            </tr>
+            <tr>
+              <td width='50%'>Compra de PC</td>
+              <td>
+                <PriceHighlight variant='outcome'>- R$ 6.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>21/03/2023</td>
+            </tr>
+            <tr>
+              <td width='50%'>Despesas</td>
+              <td>
+                <PriceHighlight variant='outcome'>- R$ 875,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>21/03/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
