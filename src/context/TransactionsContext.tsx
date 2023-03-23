870e061b609e430ba9d620412131397586413fa0
Jean Borges <jeanborges946@gmail.com>
2023-03-23 18:17:17 -0300

    feature: Add useCallback hook to keep functions referential equality and avoid unnecessary re-rendering of components

--- src/context/TransactionsContext.tsx
+++ src/context/TransactionsContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useState } from 'react';
+import { ReactNode, useCallback, useEffect, useState } from 'react';
 import { createContext } from 'use-context-selector';
 import { api } from '../lib/axios';
 
@@ -33,26 +33,30 @@ export const TransactionContext = createContext({} as TransactionContextType);
 export function TransactionsProvider({ children }: TransactionProviderProps) {
   const [transactions, setTransactions] = useState<ITransaction[]>([]);
 
-  async function fetchTransactions(query?: string) {
+  const fetchTransactions = useCallback(async (query?: string) => {
     const response = await api.get('/transactions', {
       params: { _sort: 'createdAt', _order: 'desc', q: query },
     });
 
     setTransactions(response.data);
-  }
+  }, []);
 
-  async function createTransaction(data: CreateTransactionProps) {
-    const { description, price, category, type } = data;
-    const response = await api.post('transactions', {
-      description,
-      price,
-      category,
-      type,
-      createdAt: new Date(),
-    });
+  const createTransaction = useCallback(
+    async (data: CreateTransactionProps) => {
+      const { description, price, category, type } = data;
+      const response = await api.post('transactions', {
+        description,
+        price,
+        category,
+        type,
+        createdAt: new Date(),
+      });
 
-    setTransactions((state) => [response.data, ...state]);
-  }
+      setTransactions((state) => [response.data, ...state]);
+    },
+    []
+  );
+
   useEffect(() => {
     fetchTransactions();
   }, []);
